bb397adeb10fe813e94a205b97e8289950799479
Dan Karsenty <danchlomo@gmail.com>
2019-05-07 16:41:40 +0300

    removing useless comment

--- app.py
+++ app.py
@@ -116,7 +116,6 @@ def password_update(user_id):
 
 	return redirect( url_for('profile', user_id=user_id) )
 
-# hello word whatsup?
 
 @app.route('/profile/<int:user_id>/username', methods=['POST'])
 def username_update(user_id):
